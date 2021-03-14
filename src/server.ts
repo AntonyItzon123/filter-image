abe60e06b2e139386b0583cbb83c1bb7b449a757
System Administrator <root@Antonys-MacBook-Pro.local>
2021-03-13 22:27:59 -0500

    Review's fixes, filter image, type req and res

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,50 @@
 require('dotenv').config();
-import express from 'express';
+import express , { Request, Response } from "express";
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 const app = express();
 const port = process.env.PORT || 8080;
-  
+
 app.use(bodyParser.json());
 
-app.get("/health", (req, res, next) => {
+app.get( "/", async ( req, res ) => {
+  
+  res.send("try GET /filteredimage?image_url={{}}")
+
+} );
+
+app.get("/health", (req : Request, res : Response) => {
   res.status(200).send("Hello!");
 });
 
-app.get("/filteredimage", async (req, res, next) => {
-  await deleteLocalFiles();
-  let { image_url } = req.query;
-  let filteredImage = await filterImageFromURL(`${image_url}`)
-  res.status(200).send(`${filteredImage}`);
+app.get("/filteredimage", async (req : Request, res : Response) => {
+
+  try{
+
+    await deleteLocalFiles();
+    let { image_url } = req.query;
+    let filteredImage = await filterImageFromURL(`${image_url}`)
+    console.log(filteredImage)
+    res.status(200).sendFile(filteredImage);
+
+  }catch(ex){
+
+    res.status(422).send(ex)
+
+  }
+
 });
 
-app.delete("/filteredimage", async (req, res, next) => {
+app.delete("/filteredimage", async (req : Request, res : Response) => {
+  
   await deleteLocalFiles();
   res.status(200).send(`Local Files Deleted`);
+
 });
 
-app.get( "/", async ( req, res ) => {
-  res.send("try GET /filteredimage?image_url={{}}")
-} );
-
 app.listen(port, () => {
+  
   return console.log(`server is listening on ${port}`);
+
 });
